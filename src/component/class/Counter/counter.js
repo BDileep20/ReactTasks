3ff1ff891f5131ef60c498405941e5a1bfe8977d
BDileep20 <dileepbantupalli2003@gmail.com>
2024-05-04 13:27:16 +0530

    countertask

--- src/component/class/Counter/counter.js
+++ src/component/class/Counter/counter.js
@@ -105,42 +105,27 @@ class Counter extends Component{
     render(){
         return(
         <>
-        <div style={{flexDirection:"row",flex:1,display:"flex",flexWrap:"wrap",alignItems:'end'}} >
+        <div style={{flexDirection:"row",flex:1,display:"flex",flexWrap:"wrap",alignItems:'center'}} >
          {this.state.products.map((each)=>{
           const {image,title,Count,id}=each
             return(
              <>
-                {/* <h4>{title}</h4> */}
-                {/* <div >
-                    <p style={{border:'1px solid black',padding:'-100px -100px -10px -100px'}} >
-                <img  style={{}}  src={image} height={200} width={200}></img>
-                <h4>Title:{title}</h4>
-                <div style={{display:'flex',alignItems:'end',margin:'50px 0px 10px'}}>
-                <button style={{padding:'4px 10px'}} onClick={()=>this.changAction("Increment",id)}>+</button> 
-        <span style={{position:'relative',left:'0px',border:'2px solid black',padding:'4px 10px'}}>{Count}</span>
-         <button style={{padding:'4px 10px'} }onClick={()=>this.changAction("Decrement",id)} >-</button> 
-         </div>
-         </p>
-         </div> */}
-         {/* <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gap: '20px', justifyContent: 'center' }}> */}
+                
          <div style={{border: '1px solid white', borderRadius: '9px', padding: '20px', width: '280px',margin:'10px',boxShadow:'1px 1px 20px 1px',}}>
         <img src={image} height={200} width={200} alt="Product Image" />
         <h4>Title: {title}</h4>
-        <div  style={{ display:'flex',alignItems:'end',margin:'10px' } }>
+        <div  style={{ display:'flex',justifyContent:'center',margin:'10px' } }>
             <button style={{ padding: '4px 10px' }} onClick={() => this.changAction("Increment", id)}>+</button>
             <span style={{ position: 'relative', left: '0px', border: '2px solid black', padding: '4px 10px' }}>{Count}</span>
             <button style={{ padding: '4px 10px' }} onClick={() => this.changAction("Decrement", id)}>-</button>
         </div>
         </div>
-{/* </div> */}
+
 
                 </>
             )
          })}   
-        {/* <h1>{this.state.Message}</h1>
-        <button style={{padding:'4px 10px'}} onClick={()=>this.changAction("Increment")}>+</button> 
-        <span style={{position:'relative',left:'0px',border:'2px solid black',padding:'5px 10px'}}>{this.state.Count}</span>
-         <button style={{padding:'4px 10px'} }onClick={()=>this.changAction("Decrement")} >-</button>   */}
+       
         </div>
         </>
         
